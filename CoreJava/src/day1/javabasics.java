c850c306acb825f35b4a97b30ffffc776577d773
sakthitdev <sakthit2005@gmail.com>
2025-09-03 13:51:40 +0530

    java

--- CoreJava/src/day1/javabasics.java
+++ CoreJava/src/day1/javabasics.java
@@ -1,10 +1,14 @@
 package day1;
+import java.util.Scanner;
 
 public class javabasics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
+		
+		System.out.println("Enter your name:");
+		Scanner sc = new Scanner(System.in);
 
 	}
 
